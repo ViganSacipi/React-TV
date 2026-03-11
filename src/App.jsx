ec5b1791ed55bf77c2f53e5aed58fc1698c71b44
Vigan Sacipi <vigansacipi@mac.mynetworksettings.com>
2026-03-11 18:47:58 -0400

    Completed React TV assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-/**
- * React TV is an web streaming platform that allows users to browse
- * through the episodes of a variety of different shows.
- */
+import { useState } from "react";
+import { tvShows } from "./shows/data";
+import ShowSelection from "./shows/ShowSelection";
+import ShowDetails from "./shows/ShowDetails";
+
 export default function App() {
+  const [shows] = useState(tvShows);
+  const [selectedShow, setSelectedShow] = useState(null);
+
   return (
     <>
       <header>
-        <p>React TV</p>
+        <h1>React TV</h1>
+        <ShowSelection shows={shows} setSelectedShow={setSelectedShow} />
       </header>
-      <main></main>
+
+      <main>
+        <ShowDetails show={selectedShow} />
+      </main>
     </>
   );
 }
